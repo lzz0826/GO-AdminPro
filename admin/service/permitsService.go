c6c8a30135ae0ea8c0fe8e9e8c3d32c4bb3678ce
軒 <lzz00826@gmail.com>
2024-01-18 16:01:08 +0800

    add

--- admin/service/permitsService.go
+++ admin/service/permitsService.go
@@ -5,6 +5,29 @@ import (
 	"AdminPro/dao/service/admin"
 )
 
+// GetAllPermitByAdminId 查詢指定 adminId 所有的權限 (包含持有角色下)
+func GetAllPermitByAdminId(adminId string) (permits []adminDao.PermitDAO, err error) {
+
+	//依照角色 的權限
+	var roleIds []string
+	roles, err := GetRoleByAdminId(adminId)
+	if err != nil {
+		return
+	}
+	for _, role := range roles {
+		roleIds = append(roleIds, role.ID)
+	}
+
+	// 获取角色的权限
+	roledPermits, err := GetPermitsByRoleIds(roleIds)
+	permits = append(permits, roledPermits...)
+
+	// 获取额外的权限
+	adminPermits, err := GetPermitsByAdminId(adminId)
+	permits = append(permits, adminPermits...)
+	return
+}
+
 // GetPermitsByAdminId 查詢指定 adminId 包含的 permit
 func GetPermitsByAdminId(adminId string) (permits []adminDao.PermitDAO, err error) {
 	var permitIdList []string
